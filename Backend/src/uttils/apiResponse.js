9b5d7af581fe98c81cf631d63a99e1b2f2993a5a
Muhammad Usman <m.usman14271@gmail.com>
2026-07-31 00:49:45 +0500

    API error and response utitls

---
+++ Backend/src/uttils/apiResponse.js
@@ -0,0 +1,8 @@
+class apiResponse{
+    constructor(satusCode,data,message="success"){
+        this.statusCode =statusCode;
+        this.data = data;
+        this.message= message;
+        this.success = statusCode<400;
+}
+}
