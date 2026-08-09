de47762c64a064924faa6d0d3ed41a4170ccbd66
Muhammad Usman <m.usman14271@gmail.com>
2026-08-09 21:35:40 +0500

    Validate Middleware

---
+++ Backend/src/middlewares/validate.js
@@ -0,0 +1,35 @@
+const validate = (schema) => {
+
+    return (req, res, next) => {
+
+        const data = {
+            body: req.body,
+            params: req.params,
+            query: req.query,
+        };
+
+        const { error, value } = schema.validate(data, {
+            abortEarly: false,
+            stripUnknown: true,
+        });
+
+        if (error) {
+            return res.status(400).json({
+                success: false,
+                errors: error.details.map((err) => ({
+                    field: err.path.join("."),
+                    message: err.message,
+                })),
+            });
+        }
+
+        req.body = value.body || {};
+        req.params = value.params || {};
+        req.query = value.query || {};
+
+        next();
+    };
+
+};
+
+export default validate;
